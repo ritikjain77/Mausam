d3ff50b32b583d5e80efd1670b1ed4df8fc10bbe
ritik jain <ritikjainjhs77@gmail.com>
2021-12-26 18:50:38 +0530

    CORS added

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const express =require("express");
 const path =  require('path');
 const hbs = require('hbs');
+const cors= require('cors');
 const app = express();
 const port = process.env.PORT ||8000;
-
+app.use(cors());
 const static_path =path.join(__dirname,"public");
 const template_path =path.join(__dirname,"templates/views");
 const partials_path =path.join(__dirname,"templates/partials");
